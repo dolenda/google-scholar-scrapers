67a245bf8e22d6550ad9472b7204cbbe914166b3
dominik.lenda <dlenda1729@gmail.com>
2020-01-17 17:17:54 +0100

    Create first spreadsheet - 113 articles in which authors applied multiverse analysis.

--- processing_database.R
+++ processing_database.R
@@ -7,11 +7,11 @@ setwd(path)
 rm(path)
 
 require(xlsx)
-df <- read.xlsx2("scraper_output.xlsx", sheetIndex = 1, stringsAsFactors = F)
-
-
-# remove duplicated records by title
-df1 <- df[!duplicated(df$title),]
+# df <- read.xlsx2("scraper_output.xlsx", sheetIndex = 1, stringsAsFactors = F)
+# 
+# 
+# # remove duplicated records by title
+# df1 <- df[!duplicated(df$title),]
 
 
 # write.xlsx(df1, "articles_metadata.xlsx", row.names = F, col.names = T)
@@ -19,9 +19,19 @@ df1 <- df[!duplicated(df$title),]
 
 df <- read.xlsx("articles_reviewed.xlsx", sheetIndex = 1, stringsAsFactors = F)
 
-
 # how many applied multiverse-type analysis
 sum(as.integer(df$mvs_applied))
+
+
+df$mvs_applied <- as.integer(df$mvs_applied)
+
+
+# create dataset with articles that applied multiverse analysis
+df_only_mvs <- df[df$mvs_applied == 1, ]
+
+write.xlsx(df_only_mvs, "articles_metadata.xlsx", row.names = F, col.names = T)
+
+
 
 
 
